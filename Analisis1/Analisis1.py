90cc7c0710e144b6c62d514e27c898c9208a1993
Nicolas <nicobenpe@gmail.com>
2025-02-12 12:24:37 -0300

    Mostrando estadisticas generales

--- Analisis1/Analisis1.py
+++ Analisis1/Analisis1.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 df = pd.read_excel("C:/Users/Usuario/Desktop/Escriotrio Nico/PYTHON/PRACTICA_MIERCOLES/Analisis1/Ventas.xlsx")
 
-print (df)
+print (df.head(5)) #Visualización de las primeras 5 filas
+print(df.info()) #Info sobre el dataset
+print(df.describe()) #Estadisticas generales
